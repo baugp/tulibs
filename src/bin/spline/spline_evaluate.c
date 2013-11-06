657151bb58492c6b970a8328ea250d1d68d772a1
Ralf Kaestner <ralf.kaestner@gmail.com>
2013-11-06 17:46:24 +0100

    Fixed parser configuration handling in spline_evaluate

--- src/bin/spline/spline_evaluate.c
+++ src/bin/spline/spline_evaluate.c
@@ -23,33 +23,6 @@
 #include "config/parser.h"
 #include "spline/spline.h"
 
-config_param_t file_param = {
-  "FILE",
-  config_param_type_string,
-  "",
-  "",
-  "The name of the input file containing the spline",
-};
-
-config_param_t step_size_param = {
-  "STEP_SIZE",
-  config_param_type_float,
-  "",
-  "(0.0, inf)",
-  "The step size used to generate equidistant arguments "
-  "of the spline function",
-};
-
-config_param_t eval_type_param = {
-  "eval-type",
-  config_param_type_enum,
-  "base",
-  "base|first|second",
-  "The type of spline evaluation requested, where 'base' refers to "
-  "the base function, and 'first' or 'second' indicates the first or "
-  "second derivative, respectively",
-};
-
 int main(int argc, char **argv) {
   config_parser_t parser;
 
@@ -59,17 +32,38 @@ int main(int argc, char **argv) {
     "arguments and prints the corresponding function values to stdout. "
     "Depending on the options provided, these values may be generated "
     "from the base function or its derivatives.");
-  config_set_param(&parser.arguments, &file_param);
-  config_set_param(&parser.arguments, &step_size_param);
+  config_param_p file_param = config_set_param_value_range(
+    &parser.arguments,
+    "FILE",
+    config_param_type_string,
+    "",
+    "",
+    "The name of the input file containing the spline");
+  config_param_p step_size_param = config_set_param_value_range(
+    &parser.arguments,
+    "STEP_SIZE",
+    config_param_type_float,
+    "",
+    "(0.0, inf)",
+    "The step size used to generate equidistant arguments "
+    "of the spline function");
   config_parser_option_group_p spline_option_group =
     config_parser_add_option_group(&parser, 0, "spline-", "Spline options",
     "These options control the spline operations performed by the command.");
-  config_set_param(&spline_option_group->options, &eval_type_param);
+  config_param_p eval_type_param = config_set_param_value_range(
+    &spline_option_group->options,
+    "eval-type",
+    config_param_type_enum,
+    "base",
+    "base|first|second",
+    "The type of spline evaluation requested, where 'base' refers to "
+    "the base function, and 'first' or 'second' indicates the first or "
+    "second derivative, respectively");
   config_parser_parse(&parser, argc, argv, config_parser_exit_both);
   
-  const char* file = config_param_get_string(&file_param);
-  double step_size = config_param_get_float(&step_size_param);
-  spline_eval_type_t eval_type = config_param_get_enum(&eval_type_param);
+  const char* file = config_param_get_string(file_param);
+  double step_size = config_param_get_float(step_size_param);
+  spline_eval_type_t eval_type = config_param_get_enum(eval_type_param);
   
   spline_t spline;
   int result;
